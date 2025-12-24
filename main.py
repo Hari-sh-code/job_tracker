c7b83f7b8a71bc8766f6383d6773c85d5c5ed2a8
Hari-sh-code <harishpostbox@outlook.com>
2025-12-24 12:32:16 +0530

    UV configured

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+Books = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'maths'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'commerce'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'history'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'science'},
+]
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def first_api():
+    return Books
